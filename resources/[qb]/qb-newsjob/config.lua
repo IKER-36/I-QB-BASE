1e51aa779ff8ee071f75ab683e88bfb43d5bb68b
Broncas <moha596.mn@gmail.com>
2021-07-31 22:02:27 +0200

    Traduccion QB-NEWSJOB By Broncas
    Traduccion QB-NEWSJOB By Broncas

--- resources/[qb]/qb-newsjob/config.lua
+++ resources/[qb]/qb-newsjob/config.lua
@@ -6,15 +6,15 @@ Config.Locations = {
         coords = vector4(-597.89, -929.95, 24.0, 271.5),
     },
     ["inside"] = {
-        label = "Weazle News HQ Inside",
+        label = "Weazle News HQ Dentro",
         coords = vector4(-77.46, -833.77, 243.38, 67.5),
     },
     ["outside"] = {
-        label = "Weazle News HQ Outside",
+        label = "Weazle News HQ Fuera",
         coords = vector4(-598.25, -929.86, 23.86, 86.5),
     },
     ["vehicle"] = {
-        label = "Vehicle Storage",
+        label = "Almacenamiento de Vehículos",
         coords = vector4(-552.24, -925.61, 23.86, 242.5),
     },
 }
--- resources/[qb]/qb-newsjob/server/main.lua
+++ resources/[qb]/qb-newsjob/server/main.lua
@@ -1,11 +1,11 @@
-QBCore.Commands.Add("newscam", "Grab a news camera", {}, false, function(source, args)
+QBCore.Commands.Add("newscam", "Coge una cámara de noticias", {}, false, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player.PlayerData.job.name == "reporter" then
         TriggerClientEvent("Cam:ToggleCam", source)
     end
 end)
 
-QBCore.Commands.Add("newsmic", "Grab a news microphone", {}, false, function(source, args)
+QBCore.Commands.Add("newsmic", "Coge un micrófono de noticias", {}, false, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player.PlayerData.job.name == "reporter" then
         TriggerClientEvent("Mic:ToggleMic", source)
